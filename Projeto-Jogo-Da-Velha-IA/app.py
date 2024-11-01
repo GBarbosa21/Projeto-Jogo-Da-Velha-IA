e6c48cd032886de088970f26e44a52d2a37e185b
GBarbosa21 <99029205+GBarbosa21@users.noreply.github.com>
2024-11-01 12:12:43 -0300

    Add files via upload

--- Projeto-Jogo-Da-Velha-IA/app.py
+++ Projeto-Jogo-Da-Velha-IA/app.py
@@ -65,7 +65,7 @@ def burro_burro(jogos):
         try:
             resultado_partida = {
                 'tabuleiro': tabuleiro[1:10],  # Somente as posições do tabuleiro (1 a 9)
-                'vencedor': vencedor if vencedor is not None else "Empate",
+                'vencedor': vencedor if vencedor is not None else "0",
                 'vitorias_jog1': tabuleiro[12],
                 'vitorias_jog2': tabuleiro[14],
                 'empates': tabuleiro[13]
@@ -73,6 +73,7 @@ def burro_burro(jogos):
             resultados_partidas.append(resultado_partida)
         except Exception as e:
             print(f"Erro ao salvar o resultado da partida {tabuleiro[10] + 1}: {e}")
+    salvar_resultados_csv('resultados_tic_tac_toe.csv', resultados_partidas)
 
 def burro_campeao(jogos):
     tabuleiro = [0] * 15
@@ -124,7 +125,7 @@ def burro_campeao(jogos):
 
         resultado_partida = {
             'tabuleiro': tabuleiro[1:10],  # Somente as posições do tabuleiro (1 a 9)
-            'vencedor': vencedor if vencedor is not None else "Empate",
+            'vencedor': vencedor if vencedor is not None else "0",
             'vitorias_jog1': tabuleiro[12],
             'vitorias_jog2': tabuleiro[14],
             'empates': tabuleiro[13]
@@ -152,6 +153,7 @@ def campeao_burro(jogos):
         # Loop principal do jogo
         while vencedor is None and tabuleiro[0] < 9:
             # Jogada do campeão
+            # Jogada do campeão
             posicao, caminho = jogada_campeao(tabuleiro, campeao, caminho)
             if posicao is not None and 1 <= posicao <= 9:
                 tabuleiro[posicao] = campeao
@@ -159,6 +161,8 @@ def campeao_burro(jogos):
             else:
                 print("Erro: Posição inválida retornada pela função jogada_campeao.")
             vencedor = verificar_vencedor(tabuleiro)
+            if vencedor is not None or tabuleiro[0] >= 9:
+                break
 
             # Jogada do jogador burro
             posicao = jogada_burro()
@@ -172,7 +176,6 @@ def campeao_burro(jogos):
             # Verificar vencedor após a jogada do burro
             vencedor = verificar_vencedor(tabuleiro)
             if vencedor is not None or tabuleiro[0] >= 9:
-                imprimir_tabuleiro(tabuleiro)
                 break      
 
         if vencedor == 1:
@@ -186,7 +189,7 @@ def campeao_burro(jogos):
 
         resultado_partida = {
             'tabuleiro': tabuleiro[1:10],  # Somente as posições do tabuleiro (1 a 9)
-            'vencedor': vencedor if vencedor is not None else "Empate",
+            'vencedor': vencedor if vencedor is not None else "0",
             'vitorias_jog1': tabuleiro[12],
             'vitorias_jog2': tabuleiro[14],
             'empates': tabuleiro[13]
@@ -203,6 +206,7 @@ def inteligente_burro(jogos):
     caminho_base_conhecimento = 'base_de_conhecimento.txt'
     base_de_conhecimento = carregar_base_de_conhecimento(caminho_base_conhecimento) or {}
     # Exemplo de uso
+    jogadas_usadas = []
     resultados_partidas = []
     vitorias_jog1 = 0
     vitorias_jog2 = 0
@@ -226,6 +230,10 @@ def inteligente_burro(jogos):
             if posicao is not None and 1 <= posicao <= 9:
                 tabuleiro[posicao] = Intel
                 tabuleiro[0] += 1
+                jogadas_usadas = {
+                    'tabuleiro' : tabuleiro[1:10],
+                    'posicao' : posicao,
+                }
                 atualizar_ranking(base_de_conhecimento, tabuleiro[1:10], posicao, 0)  # 0 para jogada neutra
 
             vencedor = verificar_vencedor(tabuleiro)
